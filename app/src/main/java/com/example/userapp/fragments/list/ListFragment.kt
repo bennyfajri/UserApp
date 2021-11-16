820a573ea234d18b19c8da2333e1051512164876
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2021-11-16 15:30:06 +0700

    Show data in recyclerview

--- app/src/main/java/com/example/userapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/userapp/fragments/list/ListFragment.kt
@@ -5,19 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.userapp.R
+import com.example.userapp.data.UserViewModel
 import com.example.userapp.databinding.FragmentAddBinding
 import com.example.userapp.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
     lateinit var binding: FragmentListBinding
+    private lateinit var userViewModel: UserViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentListBinding.inflate(inflater,container, false)
+
+        // Recyclerview
+        val adapter = ListAdapter()
+        val recyclerView = binding.rvListData
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // UserViewModel
+        userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        userViewModel.readAllData.observe(viewLifecycleOwner, Observer { response ->
+            adapter.setData(response)
+        })
 
         binding.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addfragment)
